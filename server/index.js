17af5304ef669eab6588fe5593fb68acb6316c21
Your Name <you@example.com>
2019-09-22 16:32:10 -0500

    parametros a un json

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const router = express.Router();
+const users = require('./users.json');
 
 app.use(cors());
 
@@ -15,16 +16,27 @@ app.listen(3000, function() {
 });
 
 app.get('/about', function(req, res) {
-    res.send('about')
-})
+    res.send('about');
+});
 
 app.route('/book')
     .get(function(req, res) {
         res.json([{ "id": "1", "nombre": "Jose", "edad": "25", "genero": "masculino", "email": "josegonzales9871@gmail.com", "localidad": "Madrid", "telefono": "912546524" }, { "id": "2", "nombre": "Juan", "edad": "31", "genero": "masculino", "email": "juanrodriguez65465@gmail.com", "localidad": "Barcelona", "telefono": "934654654" }, { "id": "3", "nombre": "Antonio", "edad": "43", "genero": "masculino", "email": "antoni654654@gmail.com", "localidad": "Valencia", "telefono": "214748366" }, { "id": "4", "nombre": "Angelina", "edad": "35", "genero": "femenino", "email": "angelina654456@gmail.com", "localidad": "New York", "telefono": "247483647" }]);
     })
     .post(function(req, res) {
-        res.send('Add a book')
+        res.send('Add a book');
     })
     .put(function(req, res) {
-        res.json({ user: 'tobi' })
-    })
+        res.json({ user: 'tobi' });
+    });
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(item => item.id == id);
+    res.json({...user });
+});
+
+app.all('*', (req, res) => {
+    console.log(req)
+    res.status(500).json({ error: 'error message' });
+});
